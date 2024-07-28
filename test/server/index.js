56f1c33ece4651617f697dbc1aed24c59ee737e7
Tgiven <henryreid868@gmail.com>
2024-07-28 18:22:51 +0000

    Update index.js

--- test/server/index.js
+++ test/server/index.js
@@ -25,6 +25,13 @@ app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ status: 'OK' });
+});
 
 app.use("/api/tracking", trackingRouter);
 app.use("/api/auth", authenticationRouter);
